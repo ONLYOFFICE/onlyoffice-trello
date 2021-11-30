cb666da69a014a13e068a4d8c82c0cda771adb5c
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2021-11-30 17:26:33 +0100

    fix: onResize handler

--- client/src/components/card-button/file/File.tsx
+++ client/src/components/card-button/file/File.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 //TODO: Alias
 import word from "../../../../public/images/word.svg";
@@ -8,49 +8,81 @@ export const FileContainer: React.FC = ({ children }) => {
   return (
     <>
       <div className="file_header">
-          <h2>Files</h2>
-            <div className='file_header__dropdown'>
-                <div className='file_header__dropdown__btn'>
-                  <b>
-                    Sorting
-                  </b>
-                  <a className='file_header__dropdown_arrow'></a>
-                </div>
-                <div className='file_header__dropdown__content'>
-                    <a>Name</a>
-                    <a>Size</a>
-                    <a>Type</a>
-                    <a>Last modified</a>
-                </div>
-            </div>
+        <h2>Files</h2>
+        <div className="file_header__dropdown">
+          <div className="file_header__dropdown__btn">
+            <b>Sorting</b>
+            <a className="file_header__dropdown_arrow"></a>
+          </div>
+          <div className="file_header__dropdown__content">
+            <a>Name</a>
+            <a>Size</a>
+            <a>Type</a>
+            <a>Last modified</a>
+          </div>
+        </div>
       </div>
       <div className="file_container">{children}</div>
     </>
   );
 };
 
+//TODO: Styles cleanup
 export const File = () => {
+  const [isMobile, setIsMobile] = useState(() => {
+    return window.innerWidth == 745;
+  });
+  useEffect(() => {
+    const onResize = () => {
+      setIsMobile(window.innerWidth <= 745);
+    };
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
   return (
     <div className="file_container_item">
-    <div className="file_container_item__main">
-      <img src={word} />
-      <h2 className="file_container_item_main__desktop">File.docx</h2>
-      <div className='file_container_item__main__text'>
-        <h2>File.docx</h2>
-        <div>
-          <p className='file_container_item__main__text__item'>4.72kB</p>
-          <p className='file_container_item__main__text__item'>Updated 50 minutes ago</p>
-        </div>
-      </div>
+      {!isMobile ? (
+        <>
+          <div className="file_container_item__main">
+            <img src={word} />
+            <h2>File.docx</h2>
+          </div>
+          <div className="file_container_item__size">4.72kB</div>
+          <div className="file_container_item__updated">
+            Updated 50 minutes ago
+          </div>
+          <div className="file_container_item__controls">
+            <button>
+              <img src={download} alt="onlyoffice_download" />
+            </button>
+            <button>Edit in ONLYOFFICE</button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="file_container_item__main">
+            <img src={word} />
+            <div className="file_container_item__main__text">
+              <h2>File.docx</h2>
+              <div>
+                <p className="file_container_item__main__text__item">4.72kB</p>
+                <p className="file_container_item__main__text__item">
+                  Updated 50 minutes ago
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="file_container_item__controls">
+            <button>
+              <img src={download} alt="onlyoffice_download" />
+            </button>
+            <button>Edit in ONLYOFFICE</button>
+          </div>
+        </>
+      )}
     </div>
-    <div className="file_container_item__controls">
-      <button>
-        <img src={download} alt="onlyoffice_download" />
-      </button>
-      <button>Edit in ONLYOFFICE</button>
-    </div>
-  </div>
-      /* <div className="file_container_item__size">4.72kB</div>
-      <div className="file_container_item__updated">Updated 50 minutes ago</div> */
   );
 };
